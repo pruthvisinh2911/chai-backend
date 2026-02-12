6eb6ef94f2b668a62e107e4da0ca5f5baf5e7ad3
Pruthvisinh Solanki <solankipruthvi356@gmail.com>
2026-02-12 13:39:28 +0530

    asdfghjkl

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -234,7 +234,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json(200,req.user,"Current user fetched successfully")
+    .json(new ApiResponse (200,req.user,"Current user fetched successfully"))
 })
 
 
@@ -245,7 +245,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
         throw new apiError(400,"all feilds are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await  User.findByIdAndUpdate(
         req.user?._id,
         {
             $set:{
