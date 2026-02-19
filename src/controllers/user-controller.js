c2ac6485dedd4b33e14995a02952f5839e2aa33c
Pruthvisinh Solanki <solankipruthvi356@gmail.com>
2026-02-19 14:04:26 +0530

    done changes in the controller

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -152,7 +152,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: { refreshToken: 1 },
+            $unset: { refreshToken: 1 },
         },
         { new: true }
     );
